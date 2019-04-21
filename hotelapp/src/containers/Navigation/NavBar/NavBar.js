02f6b568b968904b225000e6a78316f971f9aab2
JackaLakos <giannispapad96@gmail.com>
2019-04-22 01:21:37 +0300

    Added dummy logo image for demo

--- hotelapp/src/containers/Navigation/NavBar/NavBar.js
+++ hotelapp/src/containers/Navigation/NavBar/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { NavLink as RouterNavLink }  from 'react-router-dom';
 import classes from './NavBar.module.css';
+import logo from '../../../assets/images/dummy_logo.png';
 import NavigationItem from '../../../components/Navigation/NavigationItem/NavigationItem';
 import {
     Collapse,
@@ -28,7 +30,11 @@ class NavBar extends React.Component {
         return (
         <div>
             <Navbar light expand="sm" className={classes.bg_color + " fixed-top"} >
-                <NavbarBrand href="/">Logo</NavbarBrand>
+                <NavbarBrand>
+                    <RouterNavLink to="/" exact>
+                        <img src={logo} style={{height: "7vh"}} alt="Hotels App" className="img-fluid img-thumbnail"/>
+                    </RouterNavLink>
+                </NavbarBrand>
                 <NavbarToggler onClick={this.toggle} />
                 <Collapse isOpen={this.state.isOpen} navbar>
                     <Nav className="ml-auto" navbar>
