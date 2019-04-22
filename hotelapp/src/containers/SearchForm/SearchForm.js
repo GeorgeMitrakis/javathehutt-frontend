5de8823c11bc8bf471ddbedea6974e106565a277
JackaLakos <giannispapad96@gmail.com>
2019-04-22 05:33:37 +0300

    Minor changes

--- hotelapp/src/containers/SearchForm/SearchForm.js
+++ hotelapp/src/containers/SearchForm/SearchForm.js
@@ -24,109 +24,109 @@ class SearchForm extends React.Component {
 
     render() {
         return (
-            <Form>
-                <FormGroup row>
-                    <Col className={classes.search_border + " m-0 p-0"} xs="12" lg="auto">
-                        <Label hidden for="destination">Destination</Label>
-                        <InputGroup>
-                            <InputGroupAddon addonType="prepend">                                        
-                                <InputGroupText className="bg-transparent border-0 text-secondary">                                         
-                                    <i className="fas fa-bed"></i>
-                                </InputGroupText>
-                            </InputGroupAddon>
-                            <input type="text" className="form-control border-0 p-0" id="destination" placeholder="Που θα θέλατε να πάτε;"/>
-                        </InputGroup>
-                    </Col>
+            <Form> 
+                <FormGroup row> 
+                    <Col className={classes.search_border + " m-0 p-0"} xs="12" lg="auto"> 
+                        <Label hidden for="destination">Destination</Label> 
+                        <InputGroup> 
+                            <InputGroupAddon addonType="prepend">                                         
+                                <InputGroupText className="bg-transparent border-0 text-secondary">                                          
+                                    <i className="fas fa-bed"></i> 
+                                </InputGroupText> 
+                            </InputGroupAddon> 
+                            <Input type="text" className="form-control border-0 p-0" id="destination" placeholder="Που θα θέλατε να πάτε;"/> 
+                        </InputGroup> 
+                    </Col> 
 
-                    <Col className={classes.search_border + " m-0 p-0"} xs="6" md="3" lg="auto">
-                        <Label hidden for="date_from">Από</Label>
-                        <InputGroup>
-                            <InputGroupAddon addonType="prepend">                                        
-                                <InputGroupText className="bg-transparent border-0 text-secondary">                                         
-                                    <i className="far fa-calendar-alt"></i>
-                                    <span className="ml-3 font-weight-bold"> Από </span>
-                                </InputGroupText>
-                            </InputGroupAddon>
-                            <input type="date" className="form-control border-0 p-0" id="date_from"/>
-                        </InputGroup>
-                    </Col>
+                    <Col className={classes.search_border + " m-0 p-0"} xs="6" md="3" lg="auto"> 
+                        <Label hidden for="date_from">Από</Label> 
+                        <InputGroup> 
+                            <InputGroupAddon addonType="prepend">                                         
+                                <InputGroupText className="bg-transparent border-0 text-secondary">                                          
+                                    <i className="far fa-calendar-alt"></i> 
+                                    <span className="ml-3 font-weight-bold"> Από </span> 
+                                </InputGroupText> 
+                            </InputGroupAddon> 
+                            <Input type="date" className="form-control border-0 p-0" id="date_from"/> 
+                        </InputGroup> 
+                    </Col> 
 
-                    <Col className={classes.search_border + " m-0 p-0"} xs="6" md="3" lg="auto">
-                        <Label hidden for="date_to">Έως</Label>
-                        <InputGroup>
-                            <InputGroupAddon addonType="prepend">                                        
-                                <InputGroupText className="bg-transparent border-0 text-secondary">                                         
-                                    <i className="far fa-calendar-alt"></i>
-                                    <span className="ml-3 font-weight-bold"> 'Εως </span>
-                                </InputGroupText>
-                            </InputGroupAddon>
-                            <input type="date" className="form-control border-0 p-0" id="date_to"/>
-                        </InputGroup>
-                    </Col>
+                    <Col className={classes.search_border + " m-0 p-0"} xs="6" md="3" lg="auto"> 
+                        <Label hidden for="date_to">Έως</Label> 
+                        <InputGroup> 
+                            <InputGroupAddon addonType="prepend">                                         
+                                <InputGroupText className="bg-transparent border-0 text-secondary">                                          
+                                    <i className="far fa-calendar-alt"></i> 
+                                    <span className="ml-3 font-weight-bold"> 'Εως </span> 
+                                </InputGroupText> 
+                            </InputGroupAddon> 
+                            <Input type="date" className="form-control border-0 p-0" id="date_to"/> 
+                        </InputGroup> 
+                    </Col> 
 
-                    <Col className={classes.search_border + " m-0 p-0"} xs="12" sm="6" md="3" lg="auto">
-                        <InputGroupButtonDropdown addonType="append" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
-                            <DropdownToggle caret id={classes.rm_btn_hl} className="rounded-0 bg-transparent border-0 text-secondary">
-                                <i class="fas fa-user mr-3"></i>                                         
-                                1 ενήλικας - 0 παιδιά {' '}
-                            </DropdownToggle>
-                            <DropdownMenu className="p-2">
-                                <div className="d-flex align-items-center">                                                
-                                    <div className="pr-5">
-                                        Δωμάτια
+                    <Col className={classes.search_border + " m-0 p-0"} xs="12" sm="6" md="3" lg="auto"> 
+                        <InputGroupButtonDropdown addonType="append" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}> 
+                            <DropdownToggle caret className="rounded-0 bg-transparent border-0 text-secondary"> 
+                                <i class="fas fa-user mr-3"></i>                                          
+                                1 ενήλικας - 0 παιδιά {' '} 
+                            </DropdownToggle> 
+                            <DropdownMenu className="p-2"> 
+                                <div className="d-flex align-items-center">                                                 
+                                    <div className="pr-5"> 
+                                        Δωμάτια 
+                                    </div>  
+                                    <div className="pr-3"> 
+                                        <button className="form-control"><i class="fas fa-minus"></i></button> 
                                     </div> 
-                                    <div className="pr-3">
-                                        <button className="form-control"><i class="fas fa-minus"></i></button>
-                                    </div>
-                                    <div className="pr-3">
-                                        26
-                                    </div>
-                                    <div className="">
-                                        <button className="form-control"><i class="fas fa-plus"></i></button>
-                                    </div>
-                                </div>
+                                    <div className="pr-3"> 
+                                        26 
+                                    </div> 
+                                    <div className=""> 
+                                        <button className="form-control"><i class="fas fa-plus"></i></button> 
+                                    </div> 
+                                </div> 
 
-                                <DropdownItem divider />
+                                <DropdownItem divider /> 
 
-                                <Row className="align-items-center">
-                                    <Col className="d-flex justify-content-start">
-                                        Ενήλικες
-                                    </Col>
+                                <Row className="align-items-center"> 
+                                    <Col className="d-flex justify-content-start"> 
+                                        Ενήλικες 
+                                    </Col> 
 
-                                    <Col className="d-flex justify-content-end">                                                
-                                        <button className="form-control"><i class="fas fa-minus"></i></button>
-                                        <div className="pr-3 pl-3 align-self-center">
-                                            28
-                                        </div>
-                                        <button className="form-control"><i class="fas fa-plus"></i></button>                        
-                                    </Col>
-                                </Row>
+                                    <Col className="d-flex justify-content-end">                                                 
+                                        <button className="form-control"><i class="fas fa-minus"></i></button> 
+                                        <div className="pr-3 pl-3 align-self-center"> 
+                                            28 
+                                        </div> 
+                                        <button className="form-control"><i class="fas fa-plus"></i></button>                         
+                                    </Col> 
+                                </Row> 
 
-                                <DropdownItem divider />
+                                <DropdownItem divider /> 
 
-                                <Row className="align-items-center">
-                                    <Col className="d-flex justify-content-start">
-                                        Παιδιά
-                                    </Col>
+                                <Row className="align-items-center"> 
+                                    <Col className="d-flex justify-content-start"> 
+                                        Παιδιά 
+                                    </Col> 
 
-                                    <Col className="d-flex justify-content-end">                                                
-                                        <button className="form-control"><i class="fas fa-minus"></i></button>
-                                        <div className="pr-3 pl-3 align-self-center">
-                                            24
-                                        </div>
-                                        <button className="form-control"><i class="fas fa-plus"></i></button>                        
-                                    </Col>
-                                </Row>
+                                    <Col className="d-flex justify-content-end">                                                 
+                                        <button className="form-control"><i class="fas fa-minus"></i></button> 
+                                        <div className="pr-3 pl-3 align-self-center"> 
+                                            24 
+                                        </div> 
+                                        <button className="form-control"><i class="fas fa-plus"></i></button>                         
+                                    </Col> 
+                                </Row> 
 
-                            </DropdownMenu>
-                        </InputGroupButtonDropdown>
-                    </Col>
+                            </DropdownMenu> 
+                        </InputGroupButtonDropdown> 
+                    </Col> 
                 
-                    <Col className={classes.search_border + " m-0 p-0"} xs="12" sm="6" md="3" lg="auto">
-                        <button id={classes.submit_btn} className="form-control  rounded-0 font-weight-bold">Αναζήτηση</button>
-                    </Col>
-                </FormGroup>
-            </Form>
+                    <Col className={classes.search_border + " m-0 p-0"} xs="12" sm="6" md="3" lg="auto"> 
+                        <button id={classes.submit_btn} className="form-control  rounded-0 font-weight-bold">Αναζήτηση</button> 
+                    </Col> 
+                </FormGroup> 
+            </Form> 
         ); 
     }
 
