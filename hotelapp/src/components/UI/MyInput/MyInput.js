79fd7bcda2ea259bd942f3d76db353b763bda59b
JackaLakos <giannispapad96@gmail.com>
2019-04-24 10:30:55 +0300

    Changed MyInput UI component to support feedback

--- hotelapp/src/components/UI/MyInput/MyInput.js
+++ hotelapp/src/components/UI/MyInput/MyInput.js
@@ -3,11 +3,26 @@ import { Label, Input, FormGroup, FormFeedback } from 'reactstrap';
 
 
 const myInput = ( props ) => {
+
+    let formFeedback = null;
+    if (props.validity === "is-valid")
+    {
+        formFeedback = (
+            <FormFeedback valid> {props.feedback} </FormFeedback>
+        )
+    }
+    else if (props.validity === "is-invalid")
+    {
+        formFeedback = (
+            <FormFeedback invalid="true"> {props.feedback} </FormFeedback>
+        )
+    }
     
     return (
         <FormGroup>
             <Label for={props.id} className="font-weight-bold small">{props.name}</Label>
-            <Input required={props.required} onChange={props.changed} value={props.value}  type={props.type} className="" id={props.id} placeholder={props.placeholder}/>
+            <Input onBlur={props.blurred} className={props.validity} onChange={props.changed} value={props.value}  type={props.type} id={props.id} placeholder={props.placeholder}/>
+            {formFeedback}
         </FormGroup>
     );
 
