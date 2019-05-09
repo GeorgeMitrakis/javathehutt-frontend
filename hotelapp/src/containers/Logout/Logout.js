f828e3be925243cd19f0cb6f5c3cfdba9ecb2170
JackaLakos <giannispapad96@gmail.com>
2019-05-09 21:19:38 +0300

    Added Logout.js

---
+++ hotelapp/src/containers/Logout/Logout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Logout extends Component {
+
+    render () {
+        return (null);
+    }
+
+    componentDidMount() {
+        this.props.logOut();
+    }
+
+}
+
+export default Logout;
