8996ec197f586c2a4d6307f03ba0fc3b2f5a21ec
JackaLakos <giannispapad96@gmail.com>
2019-05-12 23:59:51 +0300

    SignupUser almost completed

--- hotelapp/src/containers/Signup/SignupUser/SignupUser.js
+++ hotelapp/src/containers/Signup/SignupUser/SignupUser.js
@@ -6,91 +6,237 @@ import classes from './SignupUser.module.css';
 import { Form, FormGroup, Label, FormFeedback, FormText, Button, Modal, ModalHeader, ModalBody, Input } from 'reactstrap';
 import MyInput from '../../../components/UI/MyInput/MyInput';
 import SubmitBtn from '../../../components/UI/SubmitBtn/SubmitBtn';
+import { checkValidity } from '../../../Utility/Utility';
+
 
 class SignupUser extends React.Component {
 
     state = {
-
         formControls: {
             email: {
-                required: true,
+                rules: {
+                    required: true,
+                    isEmail: true
+                },
                 id: "signup_user",
                 name: "Email",
                 value: '',
-                type: "email",
-                placeholder: "example@example.com"
+                type: "text",
+                placeholder: "example@example.com",
+                feedback: null,
+                validity: ''
             },
 
             password: {
-                required: true,
+                rules: {
+                    required: true,
+                    isPassword: true,
+                    minLength: 6,
+                    maxLength: 15
+                },
                 id: "signup_user_pwd",
-                name: "Password",
+                name: "Κωδικός",
                 value: '',
                 type: "password",
-                placeholder: ''
+                placeholder: '',
+                feedback: null,
+                validity: ''
             },
 
             password1: {
-                required: true,
+                rules: {
+                    required: true,
+                    mustMatch: "password"
+                },
                 id: "signup_user_pwd_rep",
-                name: "Confirm Password",
+                name: "Επιβεβαίωση Κωδικού",
                 value: '',
                 type: "password",
-                placeholder: ''
+                placeholder: '',
+                feedback: null,
+                validity: ''
             },
 
             name: {
-                required: true,
+                rules: {
+                    required: true,
+                    onlyLetters: true,
+                    minLength: 2,
+                    maxLength: 20
+                },
                 id: "signup_user_name",
                 name: "'Oνομα",
                 value: '',
                 type: "text",
-                placeholder: "'Ονομα"
+                placeholder: "'Ονομα",
+                feedback: null,
+                validity: ''
             },
 
             surname: {
-                required: true,
+                rules: {
+                    required: true,
+                    onlyLetters: true,
+                    minLength: 2,
+                    maxLength: 20
+                },
                 id: "signup_user_surname",
                 name: "Επίθετο",
                 value: '',
                 type: "text",
-                placeholder: "Επίθετο"
-            },
-
-            type: {
-                value: "visitor"
+                placeholder: "Επίθετο",
+                feedback: null,
+                validity: ''
             }
         }
     }
 
-    inputChangedHandler = ( event, controlName ) => {
-
-        //easy immer way
-        const formControls = produce(this.state.formControls, draft => {
-            draft[controlName].value = event.target.value
-        });
-
-        this.setState({ formControls });
+    setFormField = (controlName, feedback, validity, value) => {
+        this.setState(
+            produce(draft => {
+                draft.formControls[controlName].feedback = feedback;
+                draft.formControls[controlName].validity = validity;
+                if (value)
+                {
+                    draft.formControls[controlName].value = value;
+                }
+            })
+        );
     }
 
+    checkEmailValidity = () => {
+        axios.get("http://localhost:8765/app/api/users", 
+                {
+                    params: {
+                        email: this.state.formControls.email.value
+                    }
+                }
+            )
+            .then((result) => {
+                alert("Email is ok");
+                console.log(result);
+            })
+            .catch((err) => {
+                console.log(err);
+                // this.setFormField("email", "Το συγκεκριμένο email χρησιμοποιείται ήδη από άλλον λογαριασμό", 'is-invalid', null);
+                this.setFormField("email", null, 'is-valid', null);
+            })
+    }
+
+    inputBlurredHandler = ( event, controlName ) => {
+        if ((controlName === "email") && (this.state.formControls.email.validity !== "is-invalid") && (this.state.formControls.email.value.trim() !== '' ))
+        {
+            //kane get request gia na tsekareis an to email einai piasmeno
+            this.checkEmailValidity();
+
+        }
+        else if ((controlName === "password1") && (this.state.formControls.password1.validity !== "is-invalid") && (this.state.formControls.password1.value.trim() !== '' ))
+        {
+            if (this.state.formControls.password.value !== this.state.formControls.password1.value)
+            {
+                this.setFormField("password1", "Οι κωδικοί δεν ταιριάζουν", "is-invalid", null);
+            }
+            else if (this.state.formControls.password.validity !== "is-invalid")
+            {
+                this.setFormField("password1", null, "is-valid", null);
+            }
+        }
+        else if ((controlName === "password") && (this.state.formControls.password1.value.trim() !== ''))
+        {
+            if (this.state.formControls.password.value !== this.state.formControls.password1.value)
+            {
+                this.setFormField("password1", "Οι κωδικοί δεν ταιριάζουν", "is-invalid", null);
+            }
+            else
+            {
+                this.setFormField("password1", null, "is-valid", null);
+            }
+        }
+    }
+
+    inputChangedHandler = ( event, controlName ) => { 
+        const value = event.target.value;
+        this.setState( 
+            produce(draft => { 
+                draft.formControls[controlName].value = value; 
+            }) 
+        ); 
+            
+        const res = checkValidity(value, this.state.formControls[controlName].rules);
+        if (res.report)
+        {
+            //console.log("ola eginan ok");
+            if ((controlName === "password1") || (controlName === "email"))
+            { 
+                this.setFormField(controlName, null, '', null);
+            }
+            else
+            {
+                this.setFormField(controlName, null, 'is-valid', null);
+            }
+        }
+        else
+        {
+            this.setFormField(controlName, res.msg, "is-invalid", null);
+        }
+    }
+
+    setEmailUnavailable = () => {
+        this.setFormField("email", "Το συγκεκριμένο email χρησιμοποιείται ήδη από άλλον λογαριασμό", "is-invalid", null);
+    }
+
+    setConfirmPasswordErr = () => {
+        this.setFormField("password1", "Οι κωδικοί δεν ταιριάζουν", "is-invalid", null);
+    }
 
     submitHandler = ( event ) => {
         event.preventDefault();
 
         let formData = {};
+        let formIsValid = true;
+        let errFeedBack = {};
         for ( let key in this.state.formControls ) 
         {
-            if (key === "type")
+            formData[key] = this.state.formControls[key].value;
+
+            if (this.state.formControls[key].validity === "is-valid")
             {
                 continue;
             }
-            formData[key] = this.state.formControls[key].value;
+
+            if (this.state.formControls[key].validity === "is-invalid")
+            {
+                formIsValid = false;
+                continue;
+            }
+
+            const res = checkValidity(this.state.formControls[key].value, this.state.formControls[key].rules);
+            if (!res.report)
+            {
+                formIsValid = false;
+                errFeedBack[key] = res.msg;
+            }
         }
 
+        if (!formIsValid)
+        {
+            this.setState(
+                produce(draft => {
+                    for ( let key in errFeedBack ) 
+                    {
+                        draft.formControls[key].feedback = errFeedBack[key];
+                        draft.formControls[key].validity = "is-invalid";
+                    }
+                })
+            );
+            return;
+        }
+
+        alert("Form Submitted");
         console.log(formData);
 
         axios.post(
-            "http://localhost:8765/app/api/login",
+            "http://localhost:8765/app/api/signup",
             formData,
             {
                 'Content-Type': 'application/x-www-form-urlencoded'
@@ -102,6 +248,7 @@ class SignupUser extends React.Component {
         })
         .catch((err) => {
             console.log(err);
+            this.setEmailUnavailable();
         })
     }
 
@@ -110,11 +257,6 @@ class SignupUser extends React.Component {
         const formElementsArray = [];
         for ( let key in this.state.formControls ) 
         {
-            if (key === "type")
-            {
-                continue;
-            }
-
             formElementsArray.push( {
                 id: key,
                 config: this.state.formControls[key]
@@ -128,15 +270,16 @@ class SignupUser extends React.Component {
                 name={formElement.config.name}
                 value={formElement.config.value}
                 type={formElement.config.type}
-                required={formElement.config.required}
                 placeholder={formElement.config.placeholder}
+                feedback={formElement.config.feedback}
+                validity={formElement.config.validity}
                 changed={( event ) => this.inputChangedHandler( event, formElement.id )} 
+                blurred={( event ) => this.inputBlurredHandler ( event, formElement.id )}
             />
         ));
 
         return (
             <Form onSubmit={this.submitHandler}>
-                <p className="small">Εγγραφείτε προκειμένου να έχετε πλήρη πρόσβαση σε όλες τις υπηρεσίες μας.</p>
 
                 {formFields}
 
