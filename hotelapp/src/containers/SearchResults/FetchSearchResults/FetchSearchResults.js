ddc5a09f9ecc9908f994277f431058058a16940c
JackaLakos <giannispapad96@gmail.com>
2019-07-03 22:07:23 +0300

    Added startDate, endDate props in FetchSearchResults GET request

--- hotelapp/src/containers/SearchResults/FetchSearchResults/FetchSearchResults.js
+++ hotelapp/src/containers/SearchResults/FetchSearchResults/FetchSearchResults.js
@@ -102,6 +102,8 @@ class FetchSearchResults extends React.Component {
                                                                     hasPool: this.props.searchFilters.facilities.pool,
                                                                     hasWifi: this.props.searchFilters.facilities.wifi,
                                                                     hasShauna: this.props.searchFilters.facilities.sauna,
+                                                                    startDate: this.props.searchInfo.fromDate,
+                                                                    endDate: this.props.searchInfo.toDate,
                                                                     cityName: this.props.searchInfo.destination,
                                                                     occupants: this.props.searchInfo.adults + this.props.searchInfo.children,
                                                                     pointX: 53.430957,
